1e41a5f94ba1345a4282bc343e250feca6d2e7f7
nogw <gabrielnogueiraoliveira@gmail.com>
2022-12-21 02:38:36 +0000

    fix: remove unused let

--- lib/ppx_deriving_utils.ml
+++ lib/ppx_deriving_utils.ml
@@ -8,18 +8,20 @@ let make_lid s = make_nolocation (Longident.parse s)
 
 let make_arguments = function
   | Pcstr_record fields ->
-      List.map fields ~f:(fun f -> (f.pld_name, f.pld_type))
-  | _ -> failwith "todo"
+      List.map fields ~f:(fun field -> (field.pld_name, field.pld_type))
+  | Pcstr_tuple _ -> failwith "Tuples are not supported"
 
 let make_record ~loc fields =
-  let fields = List.map fields ~f:(fun (n, _) -> (n.txt, evar n.txt)) in
-  let fields = List.map fields ~f:(fun (n, v) -> (make_lid n, v ~loc)) in
-  pexp_record ~loc fields None
+  fields
+  |> List.map ~f:(fun (n, _) -> (n.txt, evar n.txt))
+  |> List.map ~f:(fun (n, v) -> (make_lid n, v ~loc))
+  |> fun fields -> pexp_record ~loc fields None
 
 let make_label ~labels name = if labels then Labelled name.txt else Nolabel
 
 let make_func ~loc ~labels args body =
-  let aux (arg, _) body =
+  let args = fst (List.split args) in
+  let aux arg body =
     pexp_fun ~loc (make_label ~labels arg) None (ppat_var ~loc arg) body
   in
   List.fold_right ~f:aux ~init:body args
@@ -27,10 +29,9 @@ let make_func ~loc ~labels args body =
 let make_variant ~loc name body =
   pexp_construct ~loc { loc; txt = Lident name } (Some body)
 
-let make_let_name ~ptype_name name =
-  let { txt; _ } = ptype_name in
+let make_let_name name =
   let name = String.lowercase_ascii name in
-  Printf.sprintf "mk_%s_%s" txt name
+  Printf.sprintf "mk_%s" name
 
 let make_let ~name ?(pvb_attributes = []) ~value loc =
   pstr_value ~loc Nonrecursive
@@ -40,7 +41,6 @@ module Type = struct
   let make_const ~loc name = ptyp_constr ~loc { loc; txt = lident name } []
 
   let make_arrow ~loc ~labels params return =
-    let _ = labels in
     let aux (label, ty) ret =
       ptyp_arrow ~loc (make_label ~labels label) ty ret
     in
