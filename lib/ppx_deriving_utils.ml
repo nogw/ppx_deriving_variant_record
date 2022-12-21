5832c40cf7d18c50925944c3e23e2fa6c449089d
nogw <gabrielnogueiraoliveira@gmail.com>
2022-12-21 03:27:13 +0000

    add option to show the type prefix or not

--- lib/ppx_deriving_utils.ml
+++ lib/ppx_deriving_utils.ml
@@ -7,8 +7,7 @@ let make_nolocation txt = make_location txt !Ast_helper.default_loc
 let make_lid s = make_nolocation (Longident.parse s)
 
 let make_arguments = function
-  | Pcstr_record fields ->
-      List.map fields ~f:(fun field -> (field.pld_name, field.pld_type))
+  | Pcstr_record fields -> List.map fields ~f:(fun field -> (field.pld_name, field.pld_type))
   | Pcstr_tuple _ -> failwith "Tuples are not supported"
 
 let make_record ~loc fields =
@@ -21,38 +20,34 @@ let make_label ~labels name = if labels then Labelled name.txt else Nolabel
 
 let make_func ~loc ~labels args body =
   let args = fst (List.split args) in
-  let aux arg body =
-    pexp_fun ~loc (make_label ~labels arg) None (ppat_var ~loc arg) body
-  in
-  List.fold_right ~f:aux ~init:body args
+  let aux arg body = pexp_fun ~loc (make_label ~labels arg) None (ppat_var ~loc arg) body
+  in List.fold_right ~f:aux ~init:body args
 
 let make_variant ~loc name body =
   pexp_construct ~loc { loc; txt = Lident name } (Some body)
 
-let make_let_name name =
+let make_let_name ~ptype_name ~prefix name =
   let name = String.lowercase_ascii name in
-  Printf.sprintf "mk_%s" name
+  match prefix with 
+  | true -> Printf.sprintf "%s_%s" ptype_name.txt name
+  | false -> Printf.sprintf "%s" name  
 
 let make_let ~name ?(pvb_attributes = []) ~value loc =
   pstr_value ~loc Nonrecursive
     [ { pvb_pat = name; pvb_expr = value; pvb_attributes; pvb_loc = loc } ]
 
-module Type = struct
-  let make_const ~loc name = ptyp_constr ~loc { loc; txt = lident name } []
+let make_const ~loc name = ptyp_constr ~loc { loc; txt = lident name } []
 
-  let make_arrow ~loc ~labels params return =
-    let aux (label, ty) ret =
-      ptyp_arrow ~loc (make_label ~labels label) ty ret
-    in
-    List.fold_right ~f:aux ~init:return params
+let make_arrow ~loc ~labels params return =
+  let aux (label, ty) ret = ptyp_arrow ~loc (make_label ~labels label) ty ret
+  in List.fold_right ~f:aux ~init:return params
 
-  let make_sig ~name ~ty ?(pval_attributes = []) ?(pval_prim = []) loc =
-    psig_value ~loc
-      {
-        pval_loc = loc;
-        pval_name = { loc; txt = name };
-        pval_type = ty;
-        pval_attributes;
-        pval_prim;
-      }
-end
+let make_sig ~name ~ty ?(pval_attributes = []) ?(pval_prim = []) loc =
+  psig_value ~loc
+    {
+      pval_loc = loc;
+      pval_name = { loc; txt = name };
+      pval_type = ty;
+      pval_attributes;
+      pval_prim;
+    }
--- lib/ppx_deriving_variant_record.ml
+++ lib/ppx_deriving_variant_record.ml
@@ -1,6 +1,5 @@
 module List = ListLabels
 module Ut = Ppx_deriving_utils
-module Ty = Ut.Type
 open Ppxlib
 open Asttypes
 open Parsetree
@@ -8,10 +7,10 @@ open Ast_builder.Default
 
 let deriver = "mk_record"
 
-let impl_make_record ~ptype_name ~labels constructor =
+let impl_make_record ~ptype_name ~labels ~prefix constructor =
   let { txt = name; _ } = constructor.pcd_name in
   let loc = constructor.pcd_loc in
-  let new_name = Ut.make_let_name name ~ptype_name in
+  let new_name = Ut.make_let_name ~ptype_name ~prefix name in
   let new_args = Ut.make_arguments constructor.pcd_args in
   let new_recr = Ut.make_record ~loc new_args in
   let new_variant = Ut.make_variant ~loc name new_recr in
@@ -20,17 +19,17 @@ let impl_make_record ~ptype_name ~labels constructor =
     ~name:(ppat_var ~loc { loc; txt = new_name })
     ~value:new_lambda
 
-let intf_make_record ~ptype_name ~labels constructor =
+let intf_make_record ~ptype_name ~labels ~prefix constructor =
   let { txt = name; _ } = constructor.pcd_name in
   let loc = constructor.pcd_loc in
-  let new_name = Ut.make_let_name name ~ptype_name in
+  let new_name = Ut.make_let_name ~ptype_name ~prefix name in
   let new_args = Ut.make_arguments constructor.pcd_args in
-  let tyreturn = Ty.make_const ~loc ptype_name.txt in
-  Ty.make_sig loc
-    ~ty:(Ty.make_arrow ~loc ~labels new_args tyreturn)
+  let tyreturn = Ut.make_const ~loc ptype_name.txt in
+  Ut.make_sig loc
+    ~ty:(Ut.make_arrow ~loc ~labels new_args tyreturn)
     ~name:new_name
 
-let generate_impl ~ctxt (_rec_flag, type_declarations) labels =
+let generate_impl ~ctxt (_rec_flag, type_declarations) labels prefix =
   let loc = Expansion_context.Deriver.derived_item_loc ctxt in
   let aux (td : type_declaration) =
     match td with
@@ -40,11 +39,11 @@ let generate_impl ~ctxt (_rec_flag, type_declarations) labels =
         let ext = Location.error_extensionf ~loc:ptype_loc "[todo]" in
         [ Ast_builder.Default.pstr_extension ~loc ext [] ]
     | { ptype_kind = Ptype_variant decls; ptype_name; _ } ->
-        List.map decls ~f:(impl_make_record ~ptype_name ~labels)
+        List.map decls ~f:(impl_make_record ~ptype_name ~labels ~prefix)
   in
   List.map type_declarations ~f:aux |> List.concat
 
-let generate_intf ~ctxt (_rec_flag, type_declarations) labels =
+let generate_intf ~ctxt (_rec_flag, type_declarations) labels prefix =
   let loc = Expansion_context.Deriver.derived_item_loc ctxt in
   let aux (td : type_declaration) =
     match td with
@@ -54,21 +53,21 @@ let generate_intf ~ctxt (_rec_flag, type_declarations) labels =
         let ext = Location.error_extensionf ~loc:ptype_loc "[todo]" in
         [ Ast_builder.Default.psig_extension ~loc ext [] ]
     | { ptype_kind = Ptype_variant decls; ptype_name; _ } ->
-        List.map decls ~f:(intf_make_record ~ptype_name ~labels)
+        List.map decls ~f:(intf_make_record ~ptype_name ~labels ~prefix)
   in
   List.map type_declarations ~f:aux |> List.concat
 
 let str_type_decl =
-  let args = Ppxlib.Deriving.Args.(empty +> flag "labels") in
-  let deriver ~ctxt (rec_flag, type_declarations) labels =
-    generate_impl ~ctxt (rec_flag, type_declarations) labels
+  let args = Ppxlib.Deriving.Args.(empty +> flag "labels" +> flag "prefix") in
+  let deriver ~ctxt (rec_flag, type_declarations) labels prefix =
+    generate_impl ~ctxt (rec_flag, type_declarations) labels prefix
   in
   Ppxlib.Deriving.Generator.V2.make args deriver
 
 let sig_type_decl =
-  let args = Ppxlib.Deriving.Args.(empty +> flag "labels") in
-  let deriver ~ctxt (rec_flag, type_declarations) labels =
-    generate_intf ~ctxt (rec_flag, type_declarations) labels
+  let args = Ppxlib.Deriving.Args.(empty +> flag "labels" +> flag "prefix") in
+  let deriver ~ctxt (rec_flag, type_declarations) labels prefix =
+    generate_intf ~ctxt (rec_flag, type_declarations) labels prefix
   in
   Ppxlib.Deriving.Generator.V2.make args deriver
 
